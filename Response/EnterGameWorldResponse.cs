3c3fcfd33644d073d55f5babf30ad20edcfe85a8
Nathan Berman <nathan@bermans.name>
2023-07-23 23:07:19 -0700

    Added EnterGameWorld rq/rs

---
+++ Response/EnterGameWorldResponse.cs
@@ -0,0 +1,14 @@
+﻿using GameLibrary.Model;
+namespace GameLibrary.Response
+{
+    public class EnterGameWorldResponse : IResponse
+    {
+        private string _InstanceName;
+        public string InstanceName { get { return _InstanceName; } set { _InstanceName = value; } }
+
+        public EnterGameWorldResponse(string instanceName) 
+        {
+            _InstanceName = instanceName;
+        }
+    }
+}
